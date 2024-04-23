63bbb15c713a382c2553eb7bbed41edaf24dc81a
Nandhini S <140828098+nandhini-1402@users.noreply.github.com>
2024-04-23 12:55:38 +0530

    Create Leapyr.java
    Given an integer input for the year, the objective is to check whether or not the user given input “year” is a Leap year or not. In order to do so we check if the integer input satisfies the conditions for a leap year mentioned below. Therefore, we write a Java code to check and tell if it’s a leap year or not.
    Example
    Input : 2024
    Output: Leap Year

---
+++ Leapyr.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Leapyr
+{
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int year=sc.nextInt();
+		if(year%4==0 && year%100!=0||year%400==0){
+		     System.out.println("Leap Year");
+		}
+		else{
+		    System.out.println("Not Leap Year");
+		}
+	}
+}
